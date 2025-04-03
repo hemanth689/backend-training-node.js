c88dd358fc0cf8ba321af24b8c92168ecaf15ea1
hemanth819 <hbangi@gammastack.com>
2025-04-03 11:32:26 +0530

    second-commit-s

--- filter.js
+++ filter.js
@@ -13,3 +13,5 @@ const newArr1 = products.filter(p => p.name.includes("phone"));
 const newArr2 = products.filter(p => p.price>2000);
 const newArr3 = products.filter(p => { return p.price>2000 && p.colour.includes("B") });
 console.log(newArr1, newArr2, newArr3);
+
+//dsjkfjdlsjflksjdlkfjsdsd

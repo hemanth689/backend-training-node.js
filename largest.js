d526e8c8770662d2ededf4d8a47047ffdf9a20a4
Bangi Hemanth Kumar <hbangi@gammastack.com>
2025-04-07 11:34:17 +0000

    updated

--- largest.js
+++ largest.js
@@ -9,17 +9,17 @@ if(value1>=value2 && value1>=value3 && value1>=value4 && value1>=value5)
     alert(`${value1} is the largest number`);
     //console.log(`${a} is the largest number`);
 }
-else if(value2>=value3 && value2>=value4 && value2>=value5 && value2>=value1)
+else if(value2>=value3 && value2>=value4 && value2>=value5)
 {
     alert(`${value2} is the largest number`);
     //console.log(`${b} is the largest number`);
 }
-else if(value3>=value4 && value3>=value5 && value3>=value1 && value3>=value2)
+else if(value3>=value4 && value3>=value5)
 {
     alert(`${value3} is the largest number`);
     //console.log(`${c} is the largest number`);
 }
-else if(value4>=value5 && value4>=value1 && value4>=value2 && value4>=value3)
+else if(value4>=value5)
 {
     alert(`${value4} is the largest number`);
     //console.log(`${d} is the largest number`);
@@ -29,27 +29,3 @@ else
     alert(`${value5} is the largest number`);
     //console.log(`${e} is the largest number`);
 }
-
-//..........some optimal check..........
-//we can reduce the number of comparisons by this code.
-// // if(a>=b && a>=c && a>=d && a>=e)
-// {
-//     console.log(`${a} is the largest number`);
-// }
-// else if(b>=c && b>=d && b>=e)
-// {
-//     console.log(`${b} is the largest number`);
-// }
-// else if(c>=d && c>=e)
-// {
-//     console.log(`${c} is the largest number`);
-// }
-// else if(d>=e)
-// {
-//     console.log(`${d} is the largest number`);
-// }
-// else
-// {
-//     console.log(`${e} is the largest number`);
-// }
-

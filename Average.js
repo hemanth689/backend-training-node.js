3ee84204773e00dbb11ee3c7adef6a189ed62c59
hemanth819 <hbangi@gammastack.com>
2025-04-08 11:17:24 +0530

    validated

--- Average.js
+++ Average.js
@@ -1,30 +1,38 @@
 const details = {
     "SaiKiran" : 100,
-    "Santosh" : 75,
+    "Santosh" : "50",
     "Hemanth" : 50,
-    "Samiksha" : 80,
-    "MadhuVarun" : 100
+    "Samiksha" : NaN,
+    "MadhuVarun" : 78
 }
 
 //get the keys of object using Object.keys() method and iterate through the keys. check the marks using keys.
 Object.keys(details).forEach((key) => {
-    if(details[key]<=100 && details[key]>=90)
+    const marks = details[key];
+
+    if(typeof marks !== 'number' || isNaN(marks))     //for ensuring the numbers(validating)
+    {
+        console.log(`Invalid marks`);
+        return;                             //it skips the below statements if the marks are invalid.
+    }
+
+    if(marks<=100 && marks>=90)
     {
         console.log(`${key} got a grade A`);
     }
-    else if(details[key]<90 && details[key]>=80)
+    else if(marks<90 && marks>=80)
     {
         console.log(`${key} got a grade B`);
     }
-    else if(details[key]<80 && details[key]>=70)
+    else if(marks<80 && marks>=70)
     {
         console.log(`${key} got a grade C`);
     }
-    else if(details[key]<70 && details[key]>=60)
+    else if(marks<70 && marks>=60)
     {
         console.log(`${key} got a grade D`);
     }
-    else if(details[key]<60)
+    else if(marks<60)
     {
         console.log(`${key} got a grade E`);
     }

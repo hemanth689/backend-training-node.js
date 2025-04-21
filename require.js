11612a3fe7ad324cd8827e8dd774cb47f33addd3
hemanth819 <hbangi@gammastack.com>
2025-04-21 12:41:34 +0530

    second-commit

--- require.js
+++ require.js
@@ -6,14 +6,14 @@
 
 */
 var http = require('http');     //importing the http module to create a server
-var dt = require('./dateModule');       // ./modulename because it is in same directory
+var data = require('./dateModule');       // ./modulename because it is in same directory
 
 //This will creates a server with a function request and response.
 //here 200 is http status code which means OK, this returns when everything goes well.
 http.createServer(function (req, res) {
     res.writeHead(200, {'content-Type' : 'text/html'});
     //To print on server we use write method.
-    res.write('The current date and time is : '+dt.myDateTime());
+    res.write('The current date and time is : '+data.myDateTime());
     //It closes the response.
     res.end();
 }).listen(8080);    //server runs on port number 8080 and the url is http://localhost:8080 to see the output.

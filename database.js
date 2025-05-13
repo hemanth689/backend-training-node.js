afc4420abeda9b37fe82673c18ee19a951b808f4
hemanth819 <hbangi@gammastack.com>
2025-05-13 19:20:04 +0530

    updated

--- database.js
+++ database.js
@@ -1,8 +1,16 @@
 //Implementation of users database using sequelize.
 import { Sequelize } from 'sequelize';
+//importing .env file for using credentials.
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('usertest', 'postgres', 'gammastack', {
-  host: 'localhost',
+const dbName = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+
+const sequelize = new Sequelize(dbName, username, password, {
+  host: host,
   dialect: 'postgres',
   logging: false
 });

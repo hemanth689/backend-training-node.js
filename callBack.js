0a9449918611eddbe952d72e3418d3493938e400
hemanth689 <140374174+hemanth689@users.noreply.github.com>
2026-06-27 15:32:31 +0530

    Update callBack.js

--- callBack.js
+++ callBack.js
@@ -1,6 +1,7 @@
 /*
 callback is a function which can be passed as a argument in another function. 
 Whenever we call with name of that argument it invokes the callback function.
+-> A callback function is a function that is passed as an argument to another function and is executed later by that function.
 */
 function greet(name, callback)
 {
